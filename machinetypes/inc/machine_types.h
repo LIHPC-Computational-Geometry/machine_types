0d9f8038ee4fdae272ec86a8835d41622445ef92
nicolas le goff <nicolas.le-goff@cea.fr>
2024-02-20 20:33:08 +0100

    initial commit

---
+++ machinetypes/inc/machine_types.h
@@ -0,0 +1,12 @@
+#ifndef MACHINE_TYPES_H
+#define MACHINE_TYPES_H
+
+// This header was manufactured specifically for smooth3d/magix3d
+
+// it can be called from C code so we do not use cstdint
+#include <stdint.h>
+
+typedef int64_t int_type;
+typedef double float_type;
+
+#endif  // MACHINE_TYPES_H
